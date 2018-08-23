4b98c56aa819d9bb20442008abbcd2c67d6e463c
satyaP <sats.pasupuleti@gmail.com>
2018-08-23 18:00:36 -0400

    finalized bonds and contract design

--- contracts/fedtoken.sol
+++ contracts/fedtoken.sol
@@ -4,12 +4,12 @@ import "openzeppelin-solidity/contracts/token/ERC20/MintableToken.sol";
 contract FEDToken is MintableToken {
     string public name = "FEDToken";
     string public symbol = "FED";
-    uint8 public decimals = 10;
+    uint8 public decimals = 2;
     uint public INITIAL_SUPPLY = 500000000;
     //status of how much money accounts want to put as bonds
     double public price=1.00;
     bool public needMint=false;
-    address[uint[]] bonds; //updated via web3.js form
+    BOND[] bonds;
     //get price and compare it to dollar value
     constructor() public {
         totalSupply_ = INITIAL_SUPPLY;
@@ -21,20 +21,21 @@ contract FEDToken is MintableToken {
     }
     
     //call this function first when value of FEDToken needs to be decreased
-    function releaseBond(double _numberOfBonds) public{
+    function releaseBond(uint _numberOfBonds) public{
+        for(int i = 0 ; i<_numberOfBonds; i++) {
+            bonds[i].releaseAmount();
+        }
 
     }
     //call this function if release bonds can't
     function mintToDecrease(double _amount,address _to) public {
-        //web3.js calls this function and loops in js
-           //mint function is inherited 
-    //call mint to the number of accounts in the balances in the formula
         mint(_to,_amount); //mint requires that the owner is calling it
 
     }
     //called when value of FEDToken needs to be increased
-    function addBond(uint amount, address _bondholder) public{
-        BOND bond = new BOND(FEDToken.address);
+    function addBond(uint _amount, address _bondholder) public{
+        BOND bond = new BOND(_bondholder,_amount,address(this));
+        bonds.push(bond);
 
     }
 
@@ -52,13 +53,9 @@ contract BOND {
         owner = msg.sender;
 
     }
-    function releaseAmount() private {
+    function releaseAmount() public {
         Fed = FedToken(fedTokenAddress);
         Fed.transfer(amount,bondHolder);
-        
-    
-
-        
     }
 
 }
