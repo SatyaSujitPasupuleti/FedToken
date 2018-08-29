7f0343a41a8558f8830980ede03e4f176c22deb4
satyaP <sats.pasupuleti@gmail.com>
2018-08-28 20:54:32 -0400

    added testing points

--- test/testFedToken.js
+++ test/testFedToken.js
@@ -1,5 +1,13 @@
 var fedtoken = artifacts.require("./fedtoken");
+contract("fedtoken", async(accounts)=>{
+    it("should add bonds" , async() =>{
+
+    })
+    it("should check if it can mint tokens to another specified address" , async() =>{
+
+    })
+    it("should release the bond amount" , async() =>{
+
+    })
+});
 //test following functionalities of fedToken:
-//Check if it can add bonds
-//Check if it can mint tokens to another specified address 
-//Check if it can add a bond and then release the amount
