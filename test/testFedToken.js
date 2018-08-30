108828d0d8d5eec98ca98ad7454d4aff64414a62
satyaP <sats.pasupuleti@gmail.com>
2018-08-30 17:04:24 -0400

    finished token tests

--- test/testFedToken.js
+++ test/testFedToken.js
@@ -1,17 +1,24 @@
 var fedtoken = artifacts.require("./fedtoken");
 contract("fedtoken", async(accounts)=>{
+    let instance = await fedtoken.deployed();
+    var accounts = await web3.eth.accounts;
     it("should add bonds" , async() =>{
-        let instance = await fedtoken.deployed();
-        var accounts = await web3.eth.accounts;
+       
         instance.addBond.call(10,accounts[1]);
         assert(instance.getBondLength(),1,"bond has been successfully added to list");
 
     })
     it("should check if it can mint tokens to another specified address" , async() =>{
+        instance.mintToDecrease.call(10,accounts[2]);
+        assert(instance.balances[accounts[2]],10,"fedtoken has been correctly minted");
+        
 
     })
     it("should release the bond amount" , async() =>{
-
+        instance.addBond.call(10,accounts[1]);
+        instance.releaseBond.call(1);
+        assert(instance.getBondLength(),0,"bond has been successfully removed");
+        assert(instance.balances[accounts[1]],10, "bond has returned money to owner of bond");
     })
 });
-//test following functionalities of fedToken:
+
